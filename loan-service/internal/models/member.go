b54115116b4dfb17abdd2e2446fd628dfb515c2d
yostena16 <yostenaassefa16@gmail.com>
2026-07-19 02:39:01 +0300

    feat: add role field to member

--- loan-service/internal/models/member.go
+++ loan-service/internal/models/member.go
@@ -7,6 +7,7 @@ type Member struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Name      string    `json:"name"`
 	Email     string    `gorm:"unique;not null" json:"email"`
-	Password  string    `gorm:"not null" json:"-"`
+	Password  string    `gorm:"not null" json:"-"` //here - is like we dont include the password in JSON response
+	Role      string    `gorm:"default:member" json:"role"`
 	CreatedAt time.Time `json:"created_at"`
 }
